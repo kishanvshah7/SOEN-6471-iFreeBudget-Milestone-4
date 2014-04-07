8577079b850d834fe1ea8628c26476e836ddb9c8
Kishan <shah.kishan0007@yahoo.co.uk>
2014-04-07 19:14:53 -0400

    DuplicateCode->Refactoring-Step-3
    Step 3: After extracting super class and pulling up the validate
    method,to make to mode readable and to avoid long, complicated code we
    implemented decompose conditional and extract method.A complex
    conditional statement is decomposed and checkLength is used instead of
    the complex conditional. Then, complex code under each if statement is
    extracted into separate methods like checkAccountDuplication(resp) using
    the extract method refractoring.

--- src/net/mjrz/fm/actions/TransactionValidation.java
+++ src/net/mjrz/fm/actions/TransactionValidation.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package net.mjrz.fm.actions;
 
 import net.mjrz.fm.constants.AccountTypes;
@@ -14,43 +13,67 @@ import org.hibernate.Session;
 
 /**
  *
- * @author Musers
+ * @author vivianmichaelgerard
  */
 public class TransactionValidation {
+
     FManEntityManager em = null;
+
     public void validate(Session s, Transaction t, Account from, Account to,
-			ActionResponse resp) throws Exception {
-		String fitid = t.getFitid();
-		if (fitid != null && fitid.trim().length() > 0) {
-			if (em.fitIdExists(s, t.getInitiatorId(), t.getFromAccountId(),
-					t.getToAccountId(), t.getFitid())) {
-				resp.setErrorCode(ActionResponse.TX_EXISTS_ERROR);
-				
-			}
-		}
-		/* Basic error checking... */
-		if (t.getTxAmount().doubleValue() < 0) {
-			resp.setErrorCode(ActionResponse.INVALID_TX);
-			resp.setErrorMessage("Transaction amount must be greater than zero");
-			
-		}
-		if (from.getAccountId() == to.getAccountId()) {
-			resp.setErrorCode(ActionResponse.INVALID_TX);
-			resp.setErrorMessage("To and from accounts are same");
-			
-		}
-		if (from.getStatus() != AccountTypes.ACCOUNT_ACTIVE) {
-			resp.setErrorCode(ActionResponse.INACTIVE_ACCOUNT_OPERATION);
-			resp.setErrorMessage("Account is locked [" + from.getAccountName()
-					+ "]");
-			
-		}
-		if (to.getStatus() != AccountTypes.ACCOUNT_ACTIVE) {
-			resp.setErrorCode(ActionResponse.INACTIVE_ACCOUNT_OPERATION);
-			resp.setErrorMessage("Account is locked [" + to.getAccountName()
-					+ "]");
-		
-		}
-	}
-    
+            ActionResponse resp) throws Exception {
+        String fitid = t.getFitid();
+        boolean checkLength = fitid != null && fitid.trim().length() > 0;
+
+        if (checkLength) {
+            setErrorCode(s, t, resp);
+        }
+        /* Basic error checking... */
+        if (t.getTxAmount().doubleValue() < 0) {
+            checkTransactionAmount(resp);
+        }
+
+        if (from.getAccountId() == to.getAccountId()) {
+            checkAccountDuplication(resp);
+        }
+
+        if (from.getStatus() != AccountTypes.ACCOUNT_ACTIVE) {
+            checkFromAccountStatus(resp, from);
+        }
+
+        if (to.getStatus() != AccountTypes.ACCOUNT_ACTIVE) {
+            checkToAccountStatus(resp, to);
+        }
+
+    }
+
+    public void setErrorCode(Session s, Transaction t, ActionResponse resp) throws Exception {
+        if (em.fitIdExists(s, t.getInitiatorId(), t.getFromAccountId(), t.getToAccountId(), t.getFitid())) {
+            resp.setErrorCode(ActionResponse.TX_EXISTS_ERROR);
+
+        }
+    }
+
+    public void checkTransactionAmount(ActionResponse resp) {
+        resp.setErrorCode(ActionResponse.INVALID_TX);
+        resp.setErrorMessage("Transaction amount must be greater than zero!");
+
+    }
+
+    public void checkAccountDuplication(ActionResponse resp) {
+        resp.setErrorCode(ActionResponse.INVALID_TX);
+        resp.setErrorMessage("To and from accounts are same");
+    }
+
+    public void checkFromAccountStatus(ActionResponse resp, Account from) {
+        resp.setErrorCode(ActionResponse.INACTIVE_ACCOUNT_OPERATION);
+        resp.setErrorMessage("Account is locked [" + from.getAccountName()
+                + "]");
+    }
+
+    public void checkToAccountStatus(ActionResponse resp, Account to) {
+        resp.setErrorCode(ActionResponse.INACTIVE_ACCOUNT_OPERATION);
+        resp.setErrorMessage("Account is locked [" + to.getAccountName()
+                + "]");
+    }
+
 }
